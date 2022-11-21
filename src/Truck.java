bfc2c48670c626d87d20f0701b0d32158c43c81d
Gektorrrr <48793188+Gektorrrr@users.noreply.github.com>
2022-11-21 20:00:33 +0200

    Add files via upload

---
+++ src/Truck.java
@@ -0,0 +1,16 @@
+public class Truck implements Car {
+    private Car car;
+    public Truck(Car car){
+        this.car = car;
+    }
+
+    @Override
+    public int getSpeed() {
+        return this.car.getSpeed();
+    }
+
+    @Override
+    public int getBaggageWeight() {
+        return this.car.getBaggageWeight() + 1000;
+    }
+}
